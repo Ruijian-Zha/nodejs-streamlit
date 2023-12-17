9011d3bb6f38474adfa6e8c7e643322045c52cb4
Ruijian-Zha <1323318674@qq.com>
2023-12-17 10:33:34 -0500

    Add Feature: UI change

--- UI/run.py
+++ UI/run.py
@@ -1,19 +1,39 @@
 import streamlit as st
 import requests
 
+st.set_page_config(
+    page_title="Agent2.ai",
+    initial_sidebar_state='collapsed'
+    )
+
 # Streamlit interface
-st.title("Shopify Opener")
+st.title("Agent2.ai  ")
+
+with st.sidebar:
+    st.header("God Mode")
+    god_mode = st.checkbox("Enable God Mode", True)
 
 # Define the endpoint URL
 url = "https://verified-pangolin-terribly.ngrok-free.app/open-shopify"
 
-# When the button is clicked, make a GET request to the server
-if st.button("Open Shopify"):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            st.success("Shopify is opened in Chrome!")
-        else:
-            st.error(f"Failed to open Shopify: {response.text}")
-    except requests.exceptions.RequestException as e:
-        st.error(f"Request failed: {e}")
+
+with st.container():
+    # for message in st.session_state["messages"]:
+    #    st.markdown(message)
+
+    user_input = st.chat_input("Type something...")
+    if user_input:
+        with st.chat_message("user"):
+            st.write(user_input)
+
+        if "open shopify" in user_input.lower():
+            try:
+                response = requests.get(url)
+                if response.status_code == 200:
+                    with st.chat_message("assistant"):
+                        st.write("Shopify is now open.")
+                    st.success("Shopify is opened in Chrome!")
+                else:
+                    st.error(f"Failed to open Shopify: {response.text}")
+            except requests.exceptions.RequestException as e:
+                st.error(f"Request failed: {e}")
