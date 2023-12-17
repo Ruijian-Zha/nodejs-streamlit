6b609cfe61b0cf9b1d76f45f16efdd213e534b5f
Ruijian Zha <1323318674@qq.com>
2023-12-17 13:28:09 -0500

    server trial

--- server.js
+++ server.js
@@ -5,6 +5,13 @@
 const express = require('express');
 const { Builder } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
+const FormData = require('form-data');
+let fetch;
+(async () => {
+  fetch = (await import('node-fetch')).default;
+})();
+const fs = require('fs'); // For createReadStream
+const fsp = fs.promises; // For promise-based operations
 
 const app = express();
 const port = 3000;
@@ -28,8 +35,9 @@ async function getDriver() {
  *
  * This function takes a URL from the query string of the GET request and uses the Chrome WebDriver to open the URL in Chrome.
  */
+// https://glider-summary-urgently.ngrok-free.app
 app.get('/open-url', async (req, res) => {
-  const url = req.query.url; // Get the URL from the query string
+  const url = req.query.url;
   if (!url) {
     return res.status(400).send('No URL provided.');
   }
@@ -37,14 +45,39 @@ app.get('/open-url', async (req, res) => {
   try {
     const driver = await getDriver();
     await driver.get(url);
-    res.send(`URL is opened in Chrome: ${url}`);
+    
+    // Take a screenshot
+    const screenshot = await driver.takeScreenshot();
+    const imagePath = `screenshot.png`;
+    await fsp.writeFile(imagePath, screenshot, 'base64');
+
+    // Prepare form data for POST request
+    const formData = new FormData();
+    formData.append('image', screenshot, { filename: 'screenshot.png', contentType: 'image/png' });
+
+    // Send the screenshot to the Flask server
+    const flaskServerUrl = 'https://glider-summary-urgently.ngrok-free.app/upload'; // Flask server URL
+    const response = await fetch(flaskServerUrl, {
+      method: 'POST',
+      body: formData
+    });
+    const jsonResponse = await response.json();
+
+    // Clean up the image file after sending
+    await fsp.unlink(imagePath);
+
+    if (response.ok) {
+      res.send(`URL is opened in Chrome and screenshot uploaded: ${jsonResponse.url}`);
+    } else {
+      res.status(500).send(`An error occurred while uploading the screenshot: ${jsonResponse.error}`);
+    }
   } catch (error) {
     console.error(error);
     if (driver) {
       await driver.quit();
-      driver = null; // Reset the driver after quitting
+      driver = null;
     }
-    res.status(500).send('An error occurred while opening the URL.');
+    res.status(500).send('An error occurred while opening the URL or taking a screenshot.');
   }
 });
 
